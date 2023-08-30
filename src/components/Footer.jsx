37bbe950eb0aa5daa72819645269ad25fb299f32
heshima lunyungu <137094885+heshima243@users.noreply.github.com>
2023-08-30 18:03:19 +0200

    Update Footer.jsx

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,7 +1,9 @@
 const Footer = () => {
-    return (  <footer>
-        <p>&copy; 2023 <a href="#" className="">Heshima_Lunyungu_Julien</a>. Tous droits réservés.</p>
-        </footer> );
+    return ( <footer className="footer">
+    <div className="container">
+      <p>&copy; 2023 <a href="#" className="text-white">Heshima_Lunyungu_Julien</a>. Tous droits réservés.</p>
+    </div>
+  </footer> );
 }
  
 export default Footer;

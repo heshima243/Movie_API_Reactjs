4d95716511f0300eb8ae1b8e325563d3533bf7d7
heshima lunyungu <137094885+heshima243@users.noreply.github.com>
2023-08-30 15:47:59 +0200

    Update Footer.jsx

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,6 +1,6 @@
 const Footer = () => {
     return (  <footer>
-        <p>&copy; 2023 <a href="#" class="">Heshima_Lunyungu_Julien</a>. Tous droits réservés.</p>
+        <p>&copy; 2023 <a href="#" className="">Heshima_Lunyungu_Julien</a>. Tous droits réservés.</p>
         </footer> );
 }
  
